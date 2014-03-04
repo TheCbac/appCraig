e308ce4b1f71905c1b682a9105fa03734fd90b59
Craig Cheney <ccheney@mit.edu>
2014-03-04 12:38:17 -0500

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 AppCraig::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
-  # The priority is based upon order of creation: first created -> highest priority.
+  get "static_pages/about"
+  
+end
+
+# The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
@@ -55,4 +59,3 @@ AppCraig::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
